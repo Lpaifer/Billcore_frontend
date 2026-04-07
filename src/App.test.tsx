00d160e494640cb0b06bdda43827ff362a80506d
Lucas Paifer <paiferlucas2212@gmail.com>
2026-04-06 21:19:17 -0300

    landing_page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,13 +3,13 @@ import { MemoryRouter } from "react-router-dom";
 import { App } from "./App";
 
 describe("App", () => {
-  it("renders login page as default route", () => {
+  it("renders landing page as default route", () => {
     render(
       <MemoryRouter>
         <App />
       </MemoryRouter>
     );
 
-    expect(screen.getByRole("heading", { name: "Bem-vindo de volta!" })).toBeInTheDocument();
+    expect(screen.getByRole("heading", { name: "Tome controle de suas financas" })).toBeInTheDocument();
   });
 });
